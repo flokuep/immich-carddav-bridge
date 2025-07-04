6cad4c1e8204181165fd1951c22429b6b9c4ec08
Florian Ostertag <git@florian-ostertag.de>
2025-07-04 13:17:46 +0200

    add: scaffold sync command

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,16 @@ const program = new Command();
 program
   .name("immich-carddav-sync")
   .description(
-    "Syncs preview photos of recognized people from immich to photos of CardDAV contacts."
+    "Syncs preview pictures of recognized people from immich to pictures of CardDAV contacts."
   )
   .version("0.0.1");
+
+program
+  .command("sync")
+  .description("Run synchronisation")
+  .option(
+    "-d, --dry-run",
+    "Read people and match contacts but without transferring pictures"
+  );
 
 program.parse(process.argv);
