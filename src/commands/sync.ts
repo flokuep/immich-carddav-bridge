11816b4e152f50c401e34e891ab706989e39bf77
Florian Ostertag <flokuep@users.noreply.github.com>
2025-07-05 19:04:24 +0200

    add: dry-run to test connections and matching (#4)

--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -4,17 +4,32 @@ import { getImmichPeople, getPersonImages } from "../immich";
 import matchPeopleToContacts from "../matcher";
 import { BaseOptions } from "../types";
 
-export default async function sync(options: BaseOptions) {
+/**
+ * Synchronizes pictures of Immich people with CardDAV contact images.
+ * @param options - Base options for the synchronization process.
+ * @param dryRun - If true, the synchronization will only simulate changes without applying them. Defaults to false.
+ */
+export default async function sync(
+  options: BaseOptions,
+  dryRun: boolean = false
+) {
   try {
-    const immichPeople = await getImmichPeople(options);
-    const cardDavContacts = await getCardDavContacts(options);
-    const matchings = matchPeopleToContacts(immichPeople, cardDavContacts);
-    const thumbnails = await getPersonImages(
-      options,
-      matchings.map((entry) => entry.immich.id)
-    );
-    updateContacts(options, matchings, thumbnails);
+    const [immichPeople, cardDavContacts] = await Promise.all([
+      getImmichPeople(options),
+      getCardDavContacts(options),
+    ]);
+    const matchedEntries = matchPeopleToContacts(immichPeople, cardDavContacts);
+    if (dryRun) {
+      consola.warn("Dry run enabled. No changes will be applied.");
+      return;
+    }
+
+    const immichPersonIds = matchedEntries.map((entry) => entry.immich.id);
+    const personThumbnails = await getPersonImages(options, immichPersonIds);
+
+    await updateContacts(options, matchedEntries, personThumbnails);
+    consola.success("Synchronization completed successfully!");
   } catch (error) {
-    consola.log(error);
+    consola.error("Synchronization failed:", error);
   }
 }
